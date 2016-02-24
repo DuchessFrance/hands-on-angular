8de3b370f5a048e6505893e706416dadd86f802c
vbardales <virginie.bardales@gmail.com>
2016-02-24 10:08:04 +0100

    misc

--- client/app.js
+++ client/app.js
@@ -1,5 +1,15 @@
-angular.module('myHandsOn', [])
+angular.module('myHandsOn', [
+    'ui.router',
+  ])
   .run(function() {
     console.log('it runs');
+  })
+  .config(function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('404');
+    $stateProvider
+    .state('404', {
+      url: '/#/404',
+      templateUrl: '404.html',
+    })
   })
 ;
